f96ba95f38fb5957974cf1d897157f98cfa0511a
kevin bertaux <frbertauke@gmail.com>
2026-06-09 17:52:14 +0200

    oubliés

--- tpcolle/routes/api.php
+++ tpcolle/routes/api.php
@@ -4,12 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthApiController;
 
-// --- Routes publiques ---
 Route::post('/register', [AuthApiController::class, 'register']);
 Route::post('/login', [AuthApiController::class, 'login']);
 
-
-// --- Routes protégées (nécessitent le Bearer Token dans les headers) ---
 Route::middleware('auth:sanctum')->group(function () {
     
     // POST /api/user/logout
