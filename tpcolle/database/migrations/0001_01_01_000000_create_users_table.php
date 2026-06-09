73e4915960289f46caf638f53dea9e0b49a9b3b9
kevin bertaux <frbertauke@gmail.com>
2026-06-09 16:57:55 +0200

    e

--- tpcolle/database/migrations/0001_01_01_000000_create_users_table.php
+++ tpcolle/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,21 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('userseee', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('idUtilisateur'); // idUtilisateur
-            $table->string('nom');
-            $table->string('prenom');
-            $table->string('email')->unique();
             $table->timestamps();
         });
 
